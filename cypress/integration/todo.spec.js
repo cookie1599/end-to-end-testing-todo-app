4ac81c6987bb5a458f9d0adb323a44682875a1ad
cookie1599 <atrisaendya@gmail.com>
2021-07-23 08:49:17 +0700

    fix create todo

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -3,11 +3,6 @@ describe('TODO App > ', () => {
         cy.visit('https://my-vue-todos.netlify.app/')
     })
 
-    it('OK, there are no TODOs and input todo >', () => {
-        cy.get('.form-control')
-        .type('Playing Valorant').should('have.value', 'Playing Valorant')
-    })
-
     it('Create 2 todo', ()=>{
         cy.get('.form-control')
         .type('Nap Time').type('{enter}')
@@ -20,7 +15,7 @@ describe('TODO App > ', () => {
 
     it('Editing 1 in todo', () =>{
         cy.get('ul.list-group')
-        cy.get('li').eq(0).find('.btn-outline-primary').click().type(' And Watching Anime')
+        cy.get('li').eq(0).find('.btn-outline-primary').click().type(' And Watching Anime').type('{enter}')
     })
 
     it('Delete 1 todo',() =>{
